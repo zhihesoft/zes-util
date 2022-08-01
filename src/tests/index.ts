9b683f94a8671ed30bd9f87d8b9e747f7e670555
stanley <stanley@zhihesoft.com>
2022-08-01 19:18:40 +0800

    add http functions

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,4 +1,5 @@
 import { assert } from "chai";
+import { Http } from "../lib/http";
 import { isNullOrEmpty } from "../lib/is.null.or.empty";
 import { md5 } from "../lib/md5";
 import { parseHost } from "../lib/parse.host";
@@ -29,6 +30,21 @@ describe(`zes-util test suit`, () => {
         it(`md5`, async () => {
             const value = md5("111111");
             assert.equal(value, "96e79218965eb72c92a549dd5a330112");
+        });
+    });
+
+    describe(`http`, () => {
+        it("post", async () => {
+            const http = new Http("http://echo.jsontest.com");
+            const ret = await http.post<{ key: string, one: string }>("key/value/one/two", undefined);
+            assert.equal(ret.key, "value");
+            assert.equal(ret.one, "two");
+        });
+        it("get", async () => {
+            const http = new Http("http://echo.jsontest.com");
+            const ret = await http.get<{ key: string, one: string }>("key/value/one/two", undefined);
+            assert.equal(ret.key, "value");
+            assert.equal(ret.one, "two");
         });
     });
 });
